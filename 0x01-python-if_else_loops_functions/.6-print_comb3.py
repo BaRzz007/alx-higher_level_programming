8c4b1b5e4bbfb2403a5d703d1d4d476b7137a35a
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-22 19:01:57 +0000

    Update

--- 0x01-python-if_else_loops_functions/.6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/.6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(10):
     for j in range(1, 10):
         if i == 0 and j == 1:
             print("{}{}".format(i, j), end="")
-        elif i > j:
+        elif i > j or i == j:
             continue
         else:
             print(", {}{}".format(i, j), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 for i in range(10):
     for j in range(1, 10):
         if i == 0 and j == 1:
-            print("{}{}".format(i, j), end="")
+            print("{:02d}".format((i * 10) + j), end="")
         elif i > j or i == j:
             continue
         else:
-            print(", {}{}".format(i, j), end="")
-print()
+            print(", {:02d}".format((i * 10) + j), end="")
+print ()
