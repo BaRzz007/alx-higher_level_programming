597571bc1f9acb7fc46fe7e375a53f38419317aa
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-27 21:52:08 +0000

    Create 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,18 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 if __name__ == "__main__":
+    from sys import argv
+
+    n = 1
+    n_args = len(argv) - 1
     
+    if n_args == 0:
+        print("{:d} argument.".format(n_args))
+    elif n_args == 1:
+        print("{:d} argument:".format(n_args))
+    else:
+        print("{:d} arguments:".format(n_args))
+
+    if n_args >= 1:
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
+            
