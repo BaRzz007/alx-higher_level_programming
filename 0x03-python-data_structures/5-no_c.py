acd396b7264982dc3cf8d99d9b38c09cb2bd82f5
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-29 11:14:36 +0000

    Create 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return
+
+    new_string = ''
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        new_string += char
+    return new_string
