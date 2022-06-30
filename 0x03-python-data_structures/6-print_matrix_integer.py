e9b0d395f97f1477f4313cea8f322ed5858bf51d
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-30 13:27:24 +0000

    Create 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+        return
+    
     for i in matrix:
         for elem in i:
             print("{:d} ".format(elem), end="")
