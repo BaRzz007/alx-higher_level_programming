883e56b5f8a5f536bd16b5e0ddd8a6159569a276
Bassey Ezekiel <bazzdude007@gmail.com>
2022-07-02 09:27:17 +0000

    Create 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = ()
+
+    while len(tuple_a) <= 2 or len(tuple_b) <= 2:
+        if len(tuple_a) != 2:
+            tuple_a = tuple_a + (0,)
+        if len(tuple_b) != 2:
+            tuple_b = tuple_b + (0,)
+
+    for i in range(2):
+        result += ((tuple_a[i] + tuple_b[i]),)
+
+    return result
