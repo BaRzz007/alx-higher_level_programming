07f3573ac1ba2fe8ab0647d056c4303321d368ea
Bassey Ezekiel <bazzdude007@gmail.com>
2022-07-02 11:13:51 +0000

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    result_tuple (None, 0)
+    result_tuple = (None, 0)
     if len(sentence) > 0:
         for i, elem in enumerate(sentence):
             if i == 0:
