0a57dcb728c46ca28cd2f6869076573e7289fa1a
Bassey Ezekiel <bazzdude007@gmail.com>
2022-07-02 09:59:56 +0000

    Fixed infinite loop bug

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     result = ()
 
-    while len(tuple_a) <= 2 or len(tuple_b) <= 2:
+    while len(tuple_a) < 2 or len(tuple_b) < 2:
         if len(tuple_a) != 2:
             tuple_a = tuple_a + (0,)
         if len(tuple_b) != 2:
