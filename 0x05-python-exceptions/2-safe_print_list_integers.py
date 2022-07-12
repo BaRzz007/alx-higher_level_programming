52a07571b68984cffc064e10245502c7445e8f07
Bassey Ezekiel <bazzdude007@gmail.com>
2022-07-12 10:11:23 +0000

    Create 3-safe_print_division.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    print_count = 0
     try:
         for index in range(x):
             try:
                 print("{:d}".format(my_list[index]), end="")
+                print_count += 1
             except (ValueError, TypeError):
                 continue
-            i += 1
         print()
-        return i
+        return print_count
     except IndexError:
         raise
