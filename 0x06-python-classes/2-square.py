9687ef22ecc8917ad1afccd3621f1f3539d9c581
Ezekiel Bassey <bazzdude007@gmail.com>
2022-07-16 21:46:44 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Contains Square class"""
+
 class Square():
+    """Square class.
+
+    Args:
+        size (int): size of Square object
+    """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
