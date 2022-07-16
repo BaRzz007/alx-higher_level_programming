674c879648523cda79994b28a6cb5a5bac719fe1
Ezekiel Bassey <bazzdude007@gmail.com>
2022-07-16 21:28:33 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,10 +4,10 @@
 
 class Square:
     """Defines a Square class"""
-    
+
     def __init__(self, size):
         """Initializes a Square instance with size
-        
+
         Args:
             size: initializes size of Square object
         """
