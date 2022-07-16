996cf3548062faf0042a1d0062200a45a2e86cfa
Ezekiel Bassey <bazzdude007@gmail.com>
2022-07-16 20:06:48 +0100

    Created 1-square.py and 2-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Contains a square class definition"""
+
+class Square:
+    
+    def __init__(self, size):
+        """Initializes a Square instance with size
+        
+        Args:
+            size: initializes size of Square object
+        """
+        self.__size = size
